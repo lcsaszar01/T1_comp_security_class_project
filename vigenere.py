e21951d9a5fe383944f5a47dd2e4a0b9a04575b5
epineda2 <epineda2@nd.edu>
2023-04-09 15:55:41 -0400

    Vigenere Cipher, encryption update

--- vigenere.py
+++ vigenere.py
@@ -6,11 +6,11 @@ def vigenere_cipher():
    ############## OPTIONS FOR VIGENERE CIPHER ##############
    # Type of alphabet:
    # "standard" (letters-only), "alphanum" (letters+numbers), "all" (ASCII)
-   alpha_type = "alphanum"
+   alpha_type = "standard"
 
    # Conversion type (for letters-only option):
    # "lower" (all lowercase), "upper" (all uppercase), "default" (no conversion)
-   convert_type = "default"
+   convert_type = "upper"
 
    # Non-compliant Character Settings:
    # "ignore" (keep non-compliant characters), "remove" (remove characters)
@@ -45,7 +45,7 @@ def vigenere_cipher():
        sys.exit()
 
    # Produces the enumerated alphabet #
-   alpha_dict = dict((j,i) for i,j in enumerate(alphabet, start=1))
+   alpha_dict = dict((j,i) for i,j in enumerate(alphabet))
    print(alpha_dict);
 
    # Fetches the input file & key #
@@ -59,20 +59,44 @@ def vigenere_cipher():
    # plaintext = fileinput.input(files=(filename))
    # print(plaintext)
 
-   # Some Input --> Plaintext (string) Conversions
+
 
    # TESTING USING ONLY STRING #
    key = "apple"
    init_plaintext = "?439asdANJ#9fdas)(#$4tfcsdfsfd BEEMOVIE RULES a093njcosd0930j0fsd"
-   
-   print("Initial Key: " + key)
-   print("Initial Plaintex: " + init_plaintext)
 
-  
+   # Some Input --> Plaintext (string) Conversions #
+   # Checks if input is standard vigenere cipher
+   if (alpha_type == "standard" and convert_type == "upper"):
+       init_plaintext = init_plaintext.upper()
+       key = key.upper()
+   elif (alpha_type == "standard" and convert_type == "lower"):
+       init_plaintext = init_plaintext.lower()
+       key = key.lower()
+
+   # Checks initial input 
+   print("Initial Key: " + key)
+   print("Initial Plaintext: " + init_plaintext)
 
    # Begin Encryption Function
    def vigenere_encrypt(key, plaintext, dictionary):
        cipher_result = ""
+       max_enum = max(dictionary.values())
+       pt_enum = 0 
+       key_enum = 0
+       key_index = 0
+
+       # Error Case: Invalid Key
+       for k in key:
+           if k not in alpha_dict.keys():
+                print("Key inside encryption function is invalid!", file=sys.stderr)
+                sys.exit()
+       # Error Case: Invalid Plaintext
+
+       # Error Case: Invalid Dictionary
+
+       print("Value of key is: ", key[key_enum])
+       print("Max Dictionary Value: ", max(dictionary.values()))
        
        for c in plaintext:
            if c not in dictionary.keys():
@@ -86,13 +110,19 @@ def vigenere_cipher():
                     print("Failed to check non-compliance! Check cipher options!", file=sys.stderr)
                     sys.exit()
            else:
-               cipher_result = cipher_result + c
+               pt_enum = dictionary[c]
+               key_enum = dictionary[key[key_index]]
 
+               cipher_c = (pt_enum + key_enum) % max_enum
+               cipher_result = cipher_result + list(dictionary.keys())[list(dictionary.values()).index(cipher_c)]
+               
+               key_index = key_index + 1
+               if (key_index >= len(key)):
+                   key_index = 0
+                            
        return cipher_result
-   # Conversion: Key_enum + Plaintext_enum - 1
-   # E.g. a=1, b=2, if key_enum=1, pt_enum=2, then conv=1+2-1=2 (cipher=2=b)
+   
    # Begin Decryption Function
-
    def vigenere_decrypt(key, ciphertext):
        print("Do more stuff")
        return "dud"
