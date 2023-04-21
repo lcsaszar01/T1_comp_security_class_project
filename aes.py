281e868869b9eb0f326089ff928c900060f1acf7
Lydia Csaszar <applelid07@gmail.com>
2023-04-20 20:03:09 -0400

    Updated aes.py
    New method

--- aes.py
+++ aes.py
@@ -1,19 +1,37 @@
-#!/python/usr/bin python3
+#!/usr/bin/env python3
 
-import sys, os, subprocess, pkg_resources
-from cryptography.hazmat.primitives.ciphers.aead import AESOCB3
+import sys, os, subprocess
+from Crypto.Cipher import AES 
 
-def main(input_data):
+def main(key, message):
 
-    #Code for the AES 256bit encryption
+    #Code for the AES encryption
+    
+    #ENCRYPT
+    default_key = b"More than a hero" #key of bit size 16, means we are using the AES-124 method
+    if(len(key) == 0):
+        key = default_key;
+        
+    cipher = AES.new(default_key, AES.MODE_EAX) #
+    nonce = cipher.nonce #creation of random nonce value to ensure that it is not reused.
+    ciphertext, digest_tag = cipher.encrypt_and_digest(message)
+    
+    print('The ciphertext is:', ciphertext)
+    
+    #DECRYPT
+    cipher = AES.new(default_key, AES.MODE_EAX, nonce=nonce)
+    plaintext=cipher.decrypt(ciphertext)
+    
     try:
-        data_message=input_data
-    except: 
-        data_message = b"It's kind of fun to do the impossible. - Walt Disney."
-    aad = b"authenicated but unencrypted data."
-    key = AESOCB3.generate_key(bit_length=256)
-    aesocb = AESOCB3(key)
-    nonce = os.urandom(15)
-    encrypted_message = aesocb.encrypt(nonce,data_message,aad)
-    return aesocb.decrypt(nonce, encrypted_message, aad)
+        cipher.verify(digest_tag)
+        print("Message is authentic:", plaintext)
+    except ValueError:
+        print("Error: Wrong key please try again.")
+    
+    
+key = "";
+message = b"its a big bright beautiful tomorrow."
+main(key, message)
+  
+    
     
