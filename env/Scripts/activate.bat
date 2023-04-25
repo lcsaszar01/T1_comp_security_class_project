af96455d20f71b7f4422df54c8151757a5b6a620
epineda2 <epineda2@nd.edu>
2023-04-24 20:23:12 -0400

    Library annoyances

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Eduardo\source\repos\T1_comp_security_class_project\env
+set VIRTUAL_ENV=C:\Users\Eduardo\Source\Repos\T1_comp_security_class_project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
